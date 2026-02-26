776b09a4e2a82cf087c620a797a63eee0d9d8310
rishithachalagundla <rishirishitha53@gmail.com>
2026-02-26 16:28:43 +0530

    Create script.js

---
+++ Session-14/Receipt-Generator/script.js
@@ -0,0 +1,15 @@
+const generateReceipt = (price, tip) => {
+
+    const total = price + tip;
+
+    console.log(`
+----- RECEIPT -----
+Price : ₹${price}
+Tip   : ₹${tip}
+Total : ₹${total}
+-------------------
+    `);
+};
+
+// Test
+generateReceipt(400, 50);
